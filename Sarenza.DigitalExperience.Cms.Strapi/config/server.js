9fa6be6394ad447730ce3e109116797ea5168687
Yoann LORHO <ylorho@sarenza.com>
2020-09-23 11:36:13 +0200

    first commit

---
+++ Sarenza.DigitalExperience.Cms.Strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 2337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '19eb186919693d467ef4f742f6ea262d'),
+    },
+  },
+});
---
+++ Sarenza.DigitalExperience.Cms.Strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f802c501-b633-4bca-8f4a-a2f68725bb97'
+};
