ec074add2c9ff0d9e044207c8de2bdf387d172ba
Yoann LORHO <ylorho@sarenza.com>
2020-09-25 17:36:29 +0200

    Add GraphQL and documentation

---
+++ run.ps1
@@ -0,0 +1,2 @@
+docker volume create --name strapidb -d local
+./install.ps1
--- strapi/entrypoint.sh
+++ strapi/entrypoint.sh
@@ -3,6 +3,10 @@ set -ea
 
 # npm install -g strapi
 npm install
+
+strapi install graphql
+strapi install documentation
+
 strapi build
 
 exec "$@"
