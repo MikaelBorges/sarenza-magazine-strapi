6c9ac7e5345daf358b0f38366d4ed0e1f7568997
Fabien HAUCHECORNE <fhauchecorne@sarenza.com>
2021-03-22 12:10:26 +0100

    Add isHeadline flag and Strapi home save callback

--- strapi/Strapi/api/home/models/home.js
+++ strapi/Strapi/api/home/models/home.js
@@ -5,4 +5,19 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        async afterUpdate(response, params, data) {
+
+            /* Init new column in all existing articles */
+            // const articleList = await strapi.services.article.find();
+            // articleList.forEach(article => strapi.services.article.update({ id: article.id }, { isHeadline: false }));
+
+            /* Get existing headline and disabled it if necessary */
+            const currentHeadline = await strapi.services.article.findOne({ isHeadline: true});
+            if(currentHeadline && currentHeadline.id != data.ArticleUne) strapi.services.article.update({ id: currentHeadline.id }, { isHeadline: '' });
+            /* Set new headline if necessary */
+            if(!currentHeadline || currentHeadline.id != data.ArticleUne) strapi.services.article.update({ id: data.ArticleUne }, { isHeadline: 'true' });
+        }   
+    }    
+};
