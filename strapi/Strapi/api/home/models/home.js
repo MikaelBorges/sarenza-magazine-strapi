e7e63fb08aa530cb15c44086128999b0f545f086
Fabien HAUCHECORNE <fhauchecorne@sarenza.com>
2021-03-23 09:18:33 +0100

    Add init check on new boolean entry

--- strapi/Strapi/api/home/models/home.js
+++ strapi/Strapi/api/home/models/home.js
@@ -9,15 +9,20 @@ module.exports = {
     lifecycles: {
         async afterUpdate(response, params, data) {
 
-            /* Init new column in all existing articles */
-            // const articleList = await strapi.services.article.find();
-            // articleList.forEach(article => strapi.services.article.update({ id: article.id }, { isHeadline: false }));
+            /* Init new column to false in all existing articles if needed */
+            const articleCount = await strapi.services.article.count();
+            const updatedArticleCount = await strapi.services.article.count({ isHeadline: [true, false] });
+            
+            if(!updatedArticleCount || updatedArticleCount != articleCount){
+                const articleList = await strapi.services.article.find();
+                articleList.forEach(article => strapi.services.article.update({ id: article.id }, { isHeadline: false }));
+            }
 
             /* Get existing headline and disabled it if necessary */
             const currentHeadline = await strapi.services.article.findOne({ isHeadline: true});
-            if(currentHeadline && currentHeadline.id != data.ArticleUne) strapi.services.article.update({ id: currentHeadline.id }, { isHeadline: '' });
+            if(currentHeadline && currentHeadline.id != data.ArticleUne) strapi.services.article.update({ id: currentHeadline.id }, { isHeadline: false });
             /* Set new headline if necessary */
-            if(!currentHeadline || currentHeadline.id != data.ArticleUne) strapi.services.article.update({ id: data.ArticleUne }, { isHeadline: 'true' });
+            if(!currentHeadline || currentHeadline.id != data.ArticleUne) strapi.services.article.update({ id: data.ArticleUne }, { isHeadline: true });
         }   
     }    
 };
