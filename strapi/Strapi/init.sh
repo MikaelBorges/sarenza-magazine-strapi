ec53fdec40b2547a87601f0c68c61b257a033e02
Yolo <ylorho@sarenza.com>
2021-05-04 15:25:34 +0200

    Add plugins folder check

--- strapi/Strapi/init.sh
+++ strapi/Strapi/init.sh
@@ -2,13 +2,16 @@
 set -ea    
 rootFolder=$PWD
 
-for d in plugins/*/; do
-    cd="cd ${d}"
-    eval $cd
+if [ -d "plugins" ] 
+then
+    for d in plugins/*/; do
+        cd="cd ${d}"
+        eval $cd
 
-    if [ -f "./init.sh" ]; then
-        echo "$cd is on and ready to go"
-        dos2unix ./init.sh && chmod +x ./init.sh && ./init.sh
-        eval "cd $rootFolder"
-    fi
-done
+        if [ -f "./init.sh" ]; then
+            echo "$cd is on and ready to go"
+            dos2unix ./init.sh && chmod +x ./init.sh && ./init.sh
+            eval "cd $rootFolder"
+        fi
+    done
+fi
